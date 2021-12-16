018d56d945b14577a306410614cc9995cab55c1e
keyyubi <keyyubi@hotmail.com>
2021-12-16 05:05:42 +0300

    addActivity page updated

--- src/components/supplier/AddActivity.vue
+++ src/components/supplier/AddActivity.vue
@@ -2,102 +2,148 @@
   <div class="pa-3">
     <v-row class="fill-height">
       <v-col>
-        <v-sheet height="64">
-          <v-toolbar
-            flat
-          >
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="prev"
+        <v-stepper
+          v-model="e1"
+          vertical
+        >
+          <v-stepper-header>
+            <v-stepper-step
+              :complete="e1 > 1"
+              step="1"
             >
-              <v-icon small>
-                mdi-chevron-left
-              </v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="next"
+              Danışman Seçiniz
+            </v-stepper-step>
+
+            <v-divider />
+
+            <v-stepper-step
+              :complete="e1 > 2"
+              step="2"
             >
-              <v-icon small>
-                mdi-chevron-right
-              </v-icon>
-            </v-btn>
-            <v-toolbar-title v-if="$refs.calendar">
-              {{ $refs.calendar.title }}
-            </v-toolbar-title>
-            <v-spacer />
-            <v-autocomplete
-              v-model="selectedConsultant"
-              class="mt-5"
-              :items="consultants"
-              label="Danışman"
-            />
-          </v-toolbar>
-        </v-sheet>
-        <v-sheet height="600">
-          <v-calendar
-            ref="calendar"
-            v-model="focus"
-            color="primary"
-            :events="events"
-            :event-color="getEventColor"
-            type="month"
-            @click:event="showEvent"
-            @change="updateRange"
-          />
-          <v-menu
-            v-model="selectedOpen"
-            :close-on-content-click="false"
-            :activator="selectedElement"
-            offset-x
-          >
-            <v-card
-              color="grey lighten-4"
-              min-width="350px"
-              flat
+              Aktiviteleri giriniz
+            </v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content
+              step="1"
+              class="mx-0"
             >
-              <v-toolbar
-                :color="selectedEvent.color"
-                dark
-              >
-                <v-toolbar-title v-html="selectedEvent.name" />
-                <v-spacer />
-                <v-icon>mdi-calendar-clock</v-icon>
-              </v-toolbar>
-              <v-card-text>
-                <v-slider
-                  v-model="selectedEvent.hours"
-                  class="align-center"
-                  max="12"
-                  min="0"
-                  @change="setEventTime(selectedEvent)"
+              <v-autocomplete
+                v-model="selectedConsultant"
+                class="mt-5"
+                :items="consultants"
+                label="Danışman"
+                @change="e1 = 2"
+              />
+            </v-stepper-content>
+
+            <v-stepper-content
+              step="2"
+              class="pa-0 mx-0"
+            >
+              <v-sheet height="64">
+                <v-toolbar
+                  flat
                 >
-                  <template v-slot:append>
-                    <v-text-field
-                      v-model="selectedEvent.hours"
-                      class="mt-0 pt-0"
-                      hide-details
-                      single-line
-                      min="0"
-                      max="12"
-                      type="number"
-                      style="width: 60px"
-                    />
-                  </template>
-                </v-slider>
-              </v-card-text>
-            </v-card>
-          </v-menu>
-        </v-sheet>
+                  <v-btn
+                    fab
+                    text
+                    small
+                    color="grey darken-2"
+                    :disabled="isMinMonth"
+                    @click="prev"
+                  >
+                    <v-icon small>
+                      mdi-chevron-left
+                    </v-icon>
+                  </v-btn>
+                  <v-btn
+                    fab
+                    text
+                    small
+                    color="grey darken-2"
+                    @click="next"
+                  >
+                    <v-icon small>
+                      mdi-chevron-right
+                    </v-icon>
+                  </v-btn>
+                  <v-toolbar-title v-if="$refs.calendar">
+                    {{ $refs.calendar.title }}
+                  </v-toolbar-title>
+                  <v-spacer />
+                  <v-chip
+                    v-if="selectedConsultant != null"
+                    class="ma-2"
+                  >
+                    {{ consultants[selectedConsultant].text }}
+                  </v-chip>
+                </v-toolbar>
+              </v-sheet>
+              <v-sheet
+                height="600"
+                width="100%"
+              >
+                <v-calendar
+                  ref="calendar"
+                  v-model="focus"
+                  color="primary"
+                  :events="events"
+                  :event-color="getEventColor"
+                  @click:event="showEvent"
+                  @change="updateRange"
+                />
+                <v-menu
+                  v-model="selectedOpen"
+                  :close-on-content-click="false"
+                  :activator="selectedElement"
+                  offset-x
+                >
+                  <v-card
+                    color="grey lighten-4"
+                    min-width="350px"
+                    flat
+                  >
+                    <v-toolbar
+                      :color="selectedEvent.color"
+                      dark
+                    >
+                      <v-toolbar-title v-html="selectedEvent.name" />
+                      <v-spacer />
+                      <v-icon>mdi-calendar-clock</v-icon>
+                    </v-toolbar>
+                    <v-card-text>
+                      <v-slider
+                        v-model="selectedEvent.hours"
+                        class="align-center"
+                        max="12"
+                        min="0"
+                        @change="setEventTime(selectedEvent)"
+                      >
+                        <template v-slot:append>
+                          <v-text-field
+                            v-model="selectedEvent.hours"
+                            class="mt-0 pt-0"
+                            hide-details
+                            single-line
+                            min="0"
+                            max="12"
+                            type="number"
+                            style="width: 60px"
+                          />
+                        </template>
+                      </v-slider>
+                    </v-card-text>
+                  </v-card>
+                </v-menu>
+              </v-sheet>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="selectedConsultant != null">
       <v-col>
         <v-chip
           class="ma-2"
@@ -134,7 +180,8 @@
           Toplam Fazla Mesai: {{ totalExtraHours }} saat
         </v-chip>
       </v-col>
-      <v-col class="text-right">
+      <v-spacer />
+      <v-col class="d-flex text-right">
         <v-btn
           class="white--text mr-3"
           color="green"
@@ -142,61 +189,14 @@
         >
           Onayla
         </v-btn>
-
-        <v-dialog
-          v-model="dialog"
-          persistent
-          max-width="600px"
+        <v-btn
+          color="error"
+          dark
+          depressed
+          @click="selectedConsultant = null; e1 = 1"
         >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="error"
-              dark
-              v-bind="attrs"
-              depressed
-              v-on="on"
-            >
-              Reddet
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title class="text-h5 primary white--text">
-              Red Nedeni
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col>
-                    <v-textarea
-                      outlined
-                      name="input-7-4"
-                      label="Reason of deny"
-                      placeholder="Please enter the reason of deny"
-                      messages="Cannot be empty"
-                      :value="reasonOfDeny"
-                    />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                color="green"
-                text
-                @click="dialog = false"
-              >
-                Send
-              </v-btn>
-              <v-btn
-                color="error"
-                text
-                @click="dialog = false"
-              >
-                Cancel
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+          Vazgeç
+        </v-btn>
       </v-col>
     </v-row>
   </div>
@@ -206,14 +206,16 @@
   export default {
     name: 'AddActivity',
     data: () => ({
+      e1: 1,
       focus: '',
       dialog: false,
+      isMinMonth: true,
       reasonOfDeny: '',
       selectedEvent: {},
       selectedElement: null,
       selectedOpen: false,
       selectedMonth: 'September',
-      selectedConsultant: [],
+      selectedConsultant: null,
       consultants: [
         { value: 0, text: 'Murathan Karayaz' },
         { value: 1, text: 'Busra Yargi' },
@@ -240,9 +242,12 @@
       },
       prev () {
         this.$refs.calendar.prev()
+        const current = new Date(this.$refs.calendar.value)
+        this.isMinMonth = new Date(current.getFullYear(), current.getMonth() - 1, 1) < new Date()
       },
       next () {
         this.$refs.calendar.next()
+        this.isMinMonth = false
       },
       showEvent ({ nativeEvent, event }) {
         const open = () => {
